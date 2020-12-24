9db6d6c6ecf5092d34c0c71402a2a5307d907bc7
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-12-23 21:00:37 -0600

    the first solution

---
+++ edabit_solutions.js
@@ -0,0 +1,10 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+function addition(a, b) {
+    return a + b;
+}
+
+console.log(addition(2, 3), 5);
+console.log(addition(-3, -6), -9);
+console.log(addition(7, 3), 10);
+console.log(addition(88, 2), 90);
