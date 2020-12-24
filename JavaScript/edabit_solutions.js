bc450711f0e5201dd3aff55ed1d0454f1fd8b821
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-12-23 23:26:42 -0600

    after more problems solved..

--- JavaScript/edabit_solutions.js
+++ JavaScript/edabit_solutions.js
@@ -84,7 +84,7 @@ console.log(getFirstValue([-52320, 0, 50]), -52320)
 // function hello() {
 //
 // }
-// All you have to do is type return "hello edabit.com" between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+// All you have to do is type return "hello edabit.com" between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens..
 
 function hello() {
     return "hello" + " edabit.com"
